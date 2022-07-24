57a427ec6a9ab1127e9b5703a8ce2471cb95c403
Francesca Galletti <francesca.galletti@pipefy.com>
2022-07-24 19:42:07 -0300

    Test and code checkbox funcionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByRole("button", { name: "Change to blue" });
+  expect(button).toHaveStyle({ backgroundColor: "red" });
+  fireEvent.click(button);
+  expect(button).toHaveStyle({ backgroundColor: "blue" });
+  expect(button.textContent).toBe("Change to red");
+});
+
+test("Expect checkbox to not be checked and button to be enabled", () => {
+  render(<App />);
+
+  const button = screen.getByRole("button", { name: "Change to blue" })
+  expect(button).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+})
+
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox");
+  const button = screen.getByRole("button");
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+  
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
 });
