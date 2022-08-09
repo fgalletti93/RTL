6837c6fb37282209fca6de2d82048c26ca44b588
Francesca Galletti <francesca.galletti@pipefy.com>
2022-08-09 07:54:19 -0300

    Mock service worker setpup

--- src/sundaesOnDemand/pages/Summary/SummaryForm.jsx
+++ src/sundaesOnDemand/pages/Summary/SummaryForm.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 const Sundae = () => {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
+
+
   return (
     <Form>
       <Form.Group controlId="terms-and-conditions">
@@ -20,7 +33,9 @@ const Sundae = () => {
           label={checkboxLabel}
         />
       </Form.Group>
-      <Button variant="primary" type="submit" disabled={!tcChecked}>Confirm order</Button>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
     </Form>
   );
 };
--- src/sundaesOnDemand/pages/Summary/__test__/SummaryForm.test.js
+++ src/sundaesOnDemand/pages/Summary/__test__/SummaryForm.test.js
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import '../../../../setupTests'
 import SummaryForm from "../SummaryForm.jsx";
 
 test("Should check checkbox enables and disabled button", () => {
@@ -16,13 +17,30 @@ test("Should check checkbox enables and disabled button", () => {
 test("Enables button on first click and disables on second", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
-    name:  /i agree to terms and conditions/i,
+    name: /i agree to terms and conditions/i,
   });
   const button = screen.getByRole("button", { name: "Confirm order" });
 
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeDisabled();
-})
+});
+
+test("popover responds to hover", () => {
+  render(<SummaryForm />);
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  userEvent.unhover(termsAndConditions);
+  expect(nullPopover).not.toBeInTheDocument();
+});
