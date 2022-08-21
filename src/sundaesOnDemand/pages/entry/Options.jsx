868b51a9b7acf9bcc990d6333c280dfb56041300
Francesca Galletti <francesca.galletti@pipefy.com>
2022-08-20 21:44:39 -0300

    code and test for scoops and topping options

--- src/sundaesOnDemand/pages/entry/Options.jsx
+++ src/sundaesOnDemand/pages/entry/Options.jsx
@@ -2,11 +2,11 @@ import axios from "axios";
 import { Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from './ToppingOption'
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
 
-  //optionType is scoops or toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
@@ -16,8 +16,7 @@ export default function Options({ optionType }) {
       });
   }, [optionType]);
 
-  //TODO: replace null with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
--- src/sundaesOnDemand/pages/entry/__test__/Options.test.jsx
+++ src/sundaesOnDemand/pages/entry/__test__/Options.test.jsx
@@ -4,13 +4,21 @@ import { rest } from "msw";
 import { setupServer } from "msw/node";
 import Options from "../Options";
 
-
 const server = setupServer(
   rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
         { name: "Chocolate", imagePath: "/images chocolate.png" },
         { name: "Vanilla", imagePath: "/images/vanilla.png" },
+      ])
+    );
+  }),
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Cherries", imagePath: "/images/cherries.png" },
+        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
       ])
     );
   })
@@ -26,10 +34,26 @@ test("displays image for each scoop from the server", async () => {
   render(<Options optionType="scoops" />);
 
   //find the images
-  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   //confirm alt test of images
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("display image for each topping from the server", async () => {
+  render(<Options optionType={"toppings"} />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
