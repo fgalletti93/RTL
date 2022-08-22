31eaab01f64c945c057b5a5d5f2e3a35d8bdd482
Francesca Galletti <francesca.galletti@pipefy.com>
2022-08-21 21:03:00 -0300

    adding input form to topping option

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,10 +1,26 @@
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 
 export default function ToppingOption({ name, imagePath, updateItemCount }) {
+  const handleChange = (event) => {
+    updateItemCount(name, event.target.value);
+  };
 
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
+      <Form.Group controlId={`${name}-count`} as={Row}>
+        <Form.Label>{name}</Form.Label>
+        <Col xs={5}>
+          <Form.Control
+            placeholder="0"
+            type="number"
+            defaultValue={"0"}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
       ;
     </Col>
   );
